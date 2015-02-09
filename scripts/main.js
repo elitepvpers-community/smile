3b3e64b9a8259f5274bbf77a0e2d050af6ce3ed1
Mostey <mostey1337@hotmail.de>
2015-02-09 21:51:25 +0100

    changed remote url

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var Bootstrap = {
     config: {
-      remoteUrl: 'https://rawgit.com/elitepvpers-community/smile/master/'
+      remoteUrl: 'https://rawgit.com/elitepvpers-community/smile/editmode_fix/'
     },
     init: function() { 
         var remoteScriptsUrl = Bootstrap.config.remoteUrl + "scripts/";
